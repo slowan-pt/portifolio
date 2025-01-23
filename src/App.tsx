416ead882b3dacd0e7b6e760b69d12d3f27546d5
Sloan Nascimento <sloan.nascimento@gmail.com>
2025-01-23 04:02:04 -0300

    feat: creates Hero components UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import Home from "./pages/Home/Home"
+
+
 const App = () => {
 
 
   return (
     <>
-      App
+      <Home />
     </>
   )
 }
---
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -0,0 +1,69 @@
+import { Box, Container, Grid, styled, Typography } from "@mui/material"
+//import Grid from '@mui/material/Grid2';
+import Avatar from "../../../../assets/images/avatar.png"
+import DownloadIcon from '@mui/icons-material/Download';
+import EmailIcon from '@mui/icons-material/Email';
+import StyledButton from "../../../../components/StyledButton/StyledButton";
+import { AnimatedBackground } from "../../../../components/StyledButton/AnimatedBackground/AnimatedBackground";
+
+const Hero = () => {
+
+    const StyledHero = styled("div")(({ theme }) => ({
+        backgroundColor: theme.palette.primary.main,
+        height: "100vh",
+        display: "flex",
+        alignItems: "center"
+    }))
+
+    const StyledImg = styled("img")(({theme}) => ({
+        width: "70%",
+        borderRadius: "50%",
+        justifyContent: "center",
+        border: `1ps solid ${theme.palette.primary.contrastText}`
+    }))
+
+    return (
+        <>
+            <StyledHero>
+                <Container maxWidth="lg">
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} md={5} >
+                            <Box position="relative">
+                                <Box position="absolute" width={"150%"} top={-100} right={0}>
+                                    <AnimatedBackground />
+                                </Box>
+                                <Box position="relative" textAlign="center">
+                                    <StyledImg src={Avatar} />
+                                </Box>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} md={5} >
+                            <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Sloan Nascimento</Typography>
+                            <Typography color="primary.contrastText" variant="h2" textAlign="center" >I'm a developer software</Typography>
+                            <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
+                                <Grid item xs={12} md={7} display="flex" justifyContent="center">
+                                    <StyledButton>
+                                        <DownloadIcon />
+                                        <Typography>
+                                            Download CV
+                                        </Typography>
+                                    </StyledButton>
+                                </Grid>
+                                <Grid item xs={12} md={7} display="flex" justifyContent="center">
+                                    <StyledButton>
+                                        <EmailIcon />
+                                        <Typography>
+                                            Contact me
+                                        </Typography>
+                                    </StyledButton>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </StyledHero>
+        </>
+    )
+}
+
+export default Hero
